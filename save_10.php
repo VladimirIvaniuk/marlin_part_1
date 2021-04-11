5668faf72b08fd63bde7fd16565bcd4976e25d3e
VladimirIvaniuk <ivanyuk85vladimir@gmail.com>
2021-04-11 22:47:48 +0300

    update task 10

--- save_10.php
+++ save_10.php
@@ -1,16 +1,39 @@
 <?php
 session_start();
-require_once "config/db.php";
-require_once "config/settings.php";
-$db=new DB($settings);
-$task=$db->getOne("my_table", $_POST);
+//require_once "config/db.php";
+//require_once "config/settings.php";
+//$db=new DB($settings);
+//
+//$task=$db->getOne("my_table", $_POST);
+//if(!empty($task)){
+//    $message="Введенная запись уже есть в таблице";
+//    $_SESSION['danger']=$message;
+//    header('Location: task_10.php');
+//    exit;
+//}
+//$tasks=$db->add("my_table", $_POST);
+//if(!empty($tasks)){
+//    $message="Запись добавленна";
+//    $_SESSION['success']=$message;
+//    header('Location: task_10.php');
+//    exit;
+//}
+//header('Location: task_10.php');
+$pdo=new PDO('mysql:host=localhost; dbname=marlin_part_1;', "root", "root");
+$sql ="SELECT * FROM my_table WHERE text = :text";
+$statement=$pdo->prepare($sql);
+$statement->execute(["text"=>$_POST['text']]);
+$task=$statement->fetch(PDO::FETCH_ASSOC);
 if(!empty($task)){
     $message="Введенная запись уже есть в таблице";
     $_SESSION['danger']=$message;
     header('Location: task_10.php');
     exit;
 }
-$tasks=$db->add("my_table", $_POST);
+
+$sql="INSERT INTO my_table (text) VALUES (:text)";
+$statement=$pdo->prepare($sql);
+$tasks=$statement->execute(["text"=>$_POST['text']]);
 if(!empty($tasks)){
     $message="Запись добавленна";
     $_SESSION['success']=$message;
@@ -18,5 +41,3 @@ if(!empty($tasks)){
     exit;
 }
 header('Location: task_10.php');
-?>
-<!--var_dump($_POST['text']);-->
